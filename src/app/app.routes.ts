638d8313cf8560c99069333d17ffe4d0e9e83db4
El Mariachi <emilioparodi@Boom-shakalaka-3.local>
2025-10-22 19:43:28 -0500

    certifications component setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import { PrivacyAndCookiePolicy } from './privacy-and-cookie-policy/privacy-and-cookie-policy';
 import { Home } from './home/home';
+import { Certifications } from './certifications/certifications';
+import { PrivacyAndCookiePolicy } from './privacy-and-cookie-policy/privacy-and-cookie-policy';
 
 export const routes: Routes = [
   { path: "", component: Home },
+  { path: "certifications", component: Certifications },
   { path:"privacy-and-cookie-policy", component: PrivacyAndCookiePolicy }
 ];
